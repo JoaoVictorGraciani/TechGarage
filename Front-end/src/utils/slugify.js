da4bfb6c3351d620f37530b88edf9a513ea8e7f1
gabiru421 <gwsantos18@gmail.com>
2026-08-06 21:17:45 -0300

    Delete Front-end directory

--- Front-end/src/utils/slugify.js
+++
@@ -1,8 +0,0 @@
-// src/utils/slugify.js
-export function slugify(texto) {
-    return texto
-      .toLowerCase()
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '') // remove acentos
-      .replace(/\s+/g, '-'); // espaços viram hífen
-  }
